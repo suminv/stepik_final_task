c47edcbf6b059f968d294eb1e0bb1e209e92bab7
Test User <none@email.com>
2020-08-17 16:02:02 +0200

    add test find element in page with exception

---
+++ pages/base_page.py
@@ -0,0 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage(object):
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        """ открываем нужную страницу"""
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        """будем перехватывать исключение.
+        how - как искать (css, id, xpath и тд)
+        what - что искать (строку-селектор). """
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
