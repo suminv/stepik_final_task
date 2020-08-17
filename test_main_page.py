3bf0fb409a75734bbeda898e2346659b113f8321
Test User <none@email.com>
2020-08-17 14:33:15 +0200

    init commit

--- test_main_page.py
+++ test_main_page.py
@@ -3,4 +3,7 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
-    
+
+
+
+# pytest -v --tb=line --language=en test_main_page.py
