06b5aad36823b8d8e7f5c80c1ea164dea4754dbe
Test User <none@email.com>
2020-08-25 11:32:57 +0200

    add check - > find message add item to basket.

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,9 @@
-import time
 from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    # link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
