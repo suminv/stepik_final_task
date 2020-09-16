31605d6fb0cc2efb651e284f085c7c885c758b46
Test User <none@email.com>
2020-09-16 15:07:16 +0200

    add pytest.ini

--- test_product_page.py
+++ test_product_page.py
@@ -22,18 +22,19 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.add_to_basket()
-        # page.solve_quiz_and_get_code()
+        page.solve_quiz_and_get_code()
         page.should_be_add_to_basket_message()
         page.should_be_add_to_basket_name()
         page.should_be_add_to_basket_price()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -45,6 +46,7 @@ class TestUserAddToBasketFromProductPage:
                                       "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                       marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -92,7 +94,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = LoginPage(browser, link)
@@ -101,6 +103,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     """
     Гость открывает страницу товара.
